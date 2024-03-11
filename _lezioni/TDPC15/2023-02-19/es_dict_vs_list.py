9bfdba93d9e51c6af3be4d4ad90707fae755ed23
Walter Mantovani <industrie.creative@gmail.com>
2024-03-11 08:09:33 +0100

    UPDATE: aggiunte lezioni TDPC15 + correzioni

---
+++ _lezioni/TDPC15/2023-02-19/es_dict_vs_list.py
@@ -0,0 +1,16 @@
+# CASO CON DIZIONARIO (caso più complesso)
+brani = {
+    1: 'Psycho Circus _________________________________',
+    2: 'Within ________________________________________',
+}
+
+print(brani[2])
+
+# ------------------------
+
+brani_list = [
+    'Psycho Circus _________________________________',
+    'Within ________________________________________'
+]
+
+print(brani_list[1])
---
+++ _lezioni/TDPC15/2024-02-27/esempio_strutture_dati.py
@@ -0,0 +1,10 @@
+my_tuple = (1, 3, 'pippo')
+my_list = [1, 3, {'pippo': 1123}]
+my_dict = {
+    'key': 'value',
+    1: 'pippo',
+    False: None,
+    'lista': [1, 3, 4, 5,(3, 45)]
+}
+
+my_list[2]['pippo']
