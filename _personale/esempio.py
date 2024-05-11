ae1be5661e834d57861a06531914ba67fe765467
bartolomeovarnava <161872416+bartolomeovarnava@users.noreply.github.com>
2024-05-11 14:04:07 +0000

    machine learning ricette

---
+++ _personale/esempio.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+from sklearn.pipeline import make_pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Ridge
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.model_selection import train_test_split
+
+data=pd.read_csv('ricette.csv')
+
+X = data["ingredienti"]
+y = data["gradimento"]
+
+model = make_pipeline(TfidfVectorizer(), Ridge())
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+
+model.fit(X_train, y_train) #riempie il modello
+
+score = model.score(X_test, y_test) #testa il modello
+
+nuova_ricetta = ["farina, lievito, acqua, sale, olio EVO"]
+gradimento_predetto = model.predict(nuova_ricetta)
+
+print(f'Punteggio del modello {score}')
+print(f'Puteggio predetto per {nuova_ricetta} = {gradimento_predetto}')
